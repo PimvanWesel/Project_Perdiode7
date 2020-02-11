900c34410e534e351cc87f968ddcd8a6bedc61ac
enzo marguerink <enzomarquerink@gmail.com>
2020-02-11 12:43:51 +0100

    teleporter

---
+++ Project_Periode7_Platformer/Assets/Dev/Scripts/Enzo/Teleporter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public int code;
+    float disableTimer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (disableTimer > 0)
+            disableTimer -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player" && disableTimer <= 0)
+        {
+            foreach(Teleporter tp in FindObjectOfType<Teleporter>())
+            {
+                if(tp.code==code && tp != this)
+                {
+                    tp.disableTimer = 2;
+                    Vector3 position = tp.gameObject.transform.position;
+                    position.y += 2;
+                    collider.gameObject.transform.position = position;
+                }
+            }
+        }
+    }
+}
