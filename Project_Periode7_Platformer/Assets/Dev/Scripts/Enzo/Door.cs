bbce7f62a662f2e0868f197fee4872c365934f93
Enzo <enzomarquerink@gmail.com>
2020-04-02 11:42:28 +0200

    bezig met de map en bugs fixen

---
+++ Project_Periode7_Platformer/Assets/Dev/Scripts/Enzo/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public Animation DoorOpen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            DoorOpen.GetComponent<Animation>().Play("DoorOpen");
+            Debug.Log("Door animation playing");
+        }
+    }
+}
